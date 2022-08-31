d43628acf45f950afbbf467fc90f8c3668bbf88c
Suzanne Banasihan <sjb04@ymail.com>
2022-08-31 01:05:32 -0700

    updating file

--- Assets/script.js
+++ Assets/script.js
@@ -4,7 +4,7 @@
 var generateBtn = document.querySelector("#generate");
 var password = document.querySelector("#password");
 
-// Variables for all prompts
+// Variables for the criteria prompts
 var yesNo = ["y", "n"]
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -15,23 +15,26 @@ var optionsVariable ="";
 // Event listener added to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
+// Funtion to rite password 
 function writePassword() {
   
   password.value === "";
   var randomString = "";
 
+  // Prompts for choosing criteria
   var passwordLength = prompt("Choose a length for the password between 8 and 128 characters.");
   var lowercaseLetters = prompt("Would you like to include lowercase letters in your password?(y/n)");
   var uppercaseLetters = prompt("Would you like to include uppercase letters in your password?(y/n)");
   var numbers = prompt("Would you like to include numbers in your password?(y/n)");
   var specialChar = prompt("Would you like to include special characters in your password?(y/n)");
 
+    // Alert to choose the correct character length
     if (passwordLength < 8 || passwordLength > 128) {
       alert("Your password needs to be between 8 and 128 characters.");
       var passwordLength = prompt("Choose a length for the password between 8 and 128 characters.")
     }
-
+    
+    // Alert to choose 1 or more criteria
     if (lowercaseLetters=="n" & uppercaseLetters=="n" & numbers=="n" & specialChar=="n") {
       alert("Please choose at least one criteria for the password.");
       var lowercaseLetters = prompt("Would you like to include lowercase letters in your password?(y/n)");
@@ -41,6 +44,7 @@ function writePassword() {
 
     }
 
+    //Selected criteria return a random value
     if(lowercaseLetters=="y") {
       optionsVariable += lowercase;
     }
